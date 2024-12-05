36ab560e2f1b84fabc62d61f915f297760dd06c2
Jin Kwon <onacit@gmail.com>
2024-12-05 22:01:37 +0900

    wip

--- hectofinancial-firmbanking-fulltext/src/test/java/io/github/jinahya/hectofinancial/firmbanking/fulltext/FullText_NewInstance_Test.java
+++ hectofinancial-firmbanking-fulltext/src/test/java/io/github/jinahya/hectofinancial/firmbanking/fulltext/FullText_NewInstance_Test.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,8 +29,7 @@ class FullText_NewInstance_Test {
             "getTextCategoryTextCodeAndTaskCodeArgumentsStream"
     })
     @ParameterizedTest
-    void __(final FullTextCategory category, final String textCode, final String taskCode)
-            throws IOException {
+    void __(final FullTextCategory category, final String textCode, final String taskCode) {
         final var text = FullText.newInstance(category, textCode, taskCode);
         assertThat(text).isNotNull().satisfies(t -> {
             assertThat(t.sections)
